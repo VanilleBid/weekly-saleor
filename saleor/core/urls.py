1bb5896d40b0a813fa57015a4db0e5492f0f4f7e
NyanKiyoshi <NyanKiyoshi@users.noreply.github.com>
2018-02-14 12:21:19 +0100

    Cleaned up svg pictures (+ placeholder robots.txt for debug purposes)

--- saleor/core/urls.py
+++ saleor/core/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.http import HttpResponse
 from impersonate.views import stop_impersonate
 
+from saleor import settings
 from . import views
 
 urlpatterns = [
@@ -13,3 +15,8 @@ urlpatterns = [
     url(r'^404', views.handle_404, name='handle-404'),
     url(r'^manifest\.json$', views.manifest, name='manifest'),
 ]
+
+if settings.DEBUG:
+    def robots(request):
+        return HttpResponse(b'User-agent: *\nDisallow: /', content_type='text/plain')
+    urlpatterns.append(url(r'^robots\.txt$', robots, name='robots'))
